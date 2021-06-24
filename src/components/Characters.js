e068e79bf4e452e26a2215af353b08510e4490b4
maxrabin <max.rabin17@gmail.com>
2021-06-24 12:16:28 -0400

    Done.

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,25 +1,25 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './Search'
 import IndChar from './IndChar'
 
 const Characters = () => {
-    
+
     const [characters, setCharacters] = useState([])
     const [search, setSearch] = useState('')
     useEffect(() => {
         fetch(`https://www.breakingbadapi.com/api/characters?name=${search}`)
-        .then(res => res.json())
-        .then(data => setCharacters(data))
+            .then(res => res.json())
+            .then(data => setCharacters(data))
     }, [search])
-    
+
     return (
         <div className="char-background-image">
             <Search getInput={(inp) => {
                 setSearch(inp)
-            }}/>
+            }} />
             <section className='cards'>
-                {characters.map(character => 
-                <IndChar key={character.char_id} character={character}/>  
+                {characters.map(character =>
+                    <IndChar key={character.char_id} character={character} />
                 )}
             </section>
         </div>
@@ -27,5 +27,4 @@ const Characters = () => {
 }
 
 export default Characters
-
 
